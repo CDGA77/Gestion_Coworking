21d609ec5e188a4c7e09e3b7c7e567b3044b3ef8
CDGA77 <danielgaviriaaguiar@gmail.com>
2024-06-19 17:18:02 -0500

    Se hace la insercion de los datos para cada tabla

--- coworking.sql
+++ coworking.sql
@@ -46,3 +46,74 @@ CREATE TABLE Reserva (
     FOREIGN KEY (id_espacio) REFERENCES Espacio_Trabajo(id_espacio),
     FOREIGN KEY (id_sesion) REFERENCES Sesion(id_sesion)
 );
+
+--------------////------------------------------------------////------------------------------------
+
+INSERT INTO Usuario (nombre, email, contraseña)
+SELECT
+    'Usuario ' || i,
+    'usuario' || i || '@example.com',
+    'contraseña' || i
+FROM generate_series(1, 100) i;
+
+SELECT * FROM Usuario;
+
+----------////----------------///---------
+
+-- Insertar 100 salas con 100 filas y 100 columnas cada una
+
+INSERT INTO Sala (nombre, filas, columnas)
+SELECT
+    'Sala ' || i,
+    5,
+    5
+FROM generate_series(1, 100) i;
+
+SELECT * FROM Sala;
+
+
+-------------------------/////-----------------------///---
+
+-- Insertar espacios de trabajo para cada sala
+INSERT INTO Espacio_Trabajo (fila, columna, id_sala)
+SELECT
+    fila,
+    columna,
+    s.id_sala
+FROM generate_series(1, 10) fila
+CROSS JOIN generate_series(1, 10) columna
+CROSS JOIN Sala s;
+
+SELECT * FROM espacio_trabajo;
+
+
+----------------///---------------///------------------------------
+
+-- Insertar 100 sesiones
+INSERT INTO Sesion (fecha_inicio, fecha_fin)
+SELECT
+    '2023-06-01 09:00:00'::timestamp + (i * interval '1 day'),
+    '2023-06-01 18:00:00'::timestamp + (i * interval '1 day')
+FROM generate_series(0, 100) i;
+
+SELECT * FROM Sesion
+
+------------------/////-------------------------------------------
+
+-- Insertar reservas aleatorias
+
+INSERT INTO Reserva (id_usuario, id_espacio, id_sesion, fecha_reserva)
+SELECT 
+    u.id_usuario, 
+    e.id_espacio, 
+    s.id_sesion, 
+    s.fecha_inicio - interval '1 day' + (random() * (s.fecha_fin - s.fecha_inicio + interval '1 day'))
+FROM 
+    (SELECT id_usuario FROM Usuario ORDER BY random() LIMIT 100) u
+CROSS JOIN 
+    (SELECT id_espacio FROM Espacio_Trabajo ORDER BY random() LIMIT 100) e
+CROSS JOIN 
+    (SELECT id_sesion, fecha_inicio, fecha_fin FROM Sesion ORDER BY random() LIMIT 100) s
+ORDER BY random()
+LIMIT 100;
+
